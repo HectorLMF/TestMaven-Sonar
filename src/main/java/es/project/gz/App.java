1d4abb374afcf6c483ed4f67106fe1e05a0cdd2f
Maroño <alu0100595604@ull.edu.es>
2025-10-30 12:05:28 +0000

    Saddam hussein

---
+++ src/main/java/es/project/gz/App.java
@@ -0,0 +1,16 @@
+package es.project.gz;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("A mandibula de farruquiño, non vexas como se move!");
+        System.err.println("El di que non tomou nada, pero bailalle o pasodoble");
+        System.out.println("Bailalle o pasodoble, e tamen lle castañea");
+        System.out.println("Van a ter que contratalo pra tocar nas festas da aldea");
+        System.out.println("");
+        System.out.println("Boyaka Kostova, Muiñeira de Interior, 2019");
+
+    }
+}
